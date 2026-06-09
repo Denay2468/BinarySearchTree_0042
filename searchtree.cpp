38f19b211dbe874a6e83609be47af0f1e37d6b70
Denay2468 <denayaung13@gmail.com>
2026-06-09 14:13:56 +0630

    post order function

--- searchtree.cpp
+++ searchtree.cpp
@@ -79,4 +79,16 @@ class BinaryTree{
             preorder(ptr->rightchild);
         }
     }
+
+    void postorder(Node* ptr) {
+        if (ROOT == NULL) {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != NULL) {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " ";
+        }
+    }
 };
